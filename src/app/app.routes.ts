c830f38fb8b835a172b09040cd34c14dc50be1d9
Gehad Gallo <111352486+gehad-gallo@users.noreply.github.com>
2024-08-05 14:20:03 +0300

    Add files via upload

---
+++ src/app/app.routes.ts
@@ -0,0 +1,14 @@
+import { Routes } from '@angular/router';
+import { CardComponent } from './card/card.component';
+import { NotFoundPageComponent } from './not-found-page/not-found-page.component';
+import { SinglePageProductComponent } from './single-page-product/single-page-product.component';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { SignupFormComponent } from './signup-form/signup-form.component';
+
+export const routes: Routes = [
+  { path: 'product', component: CardComponent },
+  { path: 'single-page/:id', component: SinglePageProductComponent },
+  { path: 'login', component: LoginFormComponent },
+  { path: 'signup', component: SignupFormComponent },
+  { path: '**', component: NotFoundPageComponent },
+];
